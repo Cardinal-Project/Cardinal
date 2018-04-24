90f75a1ff667204bbdeb05a07bf2ad45aa48265a
Ciborn <rob1.bourachot@gmail.com>
2018-04-25 01:10:52 +0200

    isEmpty Utils function

---
+++ core/functions/utils/isEmpty.js
@@ -0,0 +1,19 @@
+// Vérification du contenu d'un objet
+// v1.0
+
+module.exports = function(obj) {
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
+    
+    if (obj == null) return true;
+    
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+    
+    if (typeof obj !== "object") return true;
+    
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+    
+    return true;
+}
