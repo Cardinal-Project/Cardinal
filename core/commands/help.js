60fa5879f8eb761894b501df65d61f5191136032
Ciborn <rob1.bourachot@gmail.com>
2018-04-26 11:12:30 +0200

    added help command

--- core/commands/help.js
+++ core/commands/help.js
@@ -1,31 +1,35 @@
-const Discord = require('discord.js');
-const commands = require('./../data/commands.json');
-const config = require('./../../config.json');
-const GuildSettings = require('./../classes/Cache/GuildSettings');
-const permissions = require('./../data/permissions.json');
 const isEmpty = require('./../functions/utils/isEmpty');
-const UserPerms = require('./../classes/UserPerms');
+const commands = require('./../data/commands.json');
+const User = require('./../classes/Discord/User');
+const BotPerms = require('./../classes/BotPerms');
+const config = require('./../../config.json');
+const Cache = require('./../classes/Cache');
+const Args = require('./../classes/Args');
+const Discord = require('discord.js');
 module.exports = function(bot, message) {
-    const cache = new GuildSettings();
-    const args = require('./../functions/utils/args')(message.content, message.guild.id);
+    const args = new Args(message.content, ' ');
+    args.args.shift();
+    const prefix = new Cache(message.guild.id, 'guildSettings.json').get('prefix');
 
-    if (!isEmpty(args)) {
-        if (typeof commands[args[0]] != 'undefined') {
+    if (!isEmpty(args.args)) {
+        if (typeof commands[args.args[0]] != 'undefined') {
+            const command = commands[args.args[0]];
             var embed = new Discord.RichEmbed()
-                .setTitle(commands[args[0]].command)
+                .setTitle(command.name)
                 .setColor('GOLD')
-                .setDescription(commands[args[0]].shortDesc);
+                .setDescription(command.shortDesc);
             
-            if (commands[args[0]].botPerms != '' && commands[args[0]].botPerms.indexOf('USE_BOT') == -1) {
-                embed.addField('Required Bots Permissions', commands[args[0]].botPerms.replace('ADMINISTRATOR', '').trim(), true);
+            const requiredBotPerms = new BotPerms(command.botPerms);
+            if (requiredBotPerms.has('USE_BOT')) {
+                embed.addField('Required Bots Permissions', requiredBotPerms.perms.join(' '), true);
             }
             
-            if (commands[args[0]].discordPerms != '') {
-                embed.addField('Required Discord Permissions', commands[args[0]].discordPerms.replace('ADMINISTRATOR', '').trim(), true);
+            if (command.discordPerms != '') {
+                embed.addField('Required Discord Permissions', command.discordPerms.replace('ADMINISTRATOR', '').trim(), true);
             }
 
-            if (typeof commands[args[0]].fields != 'undefined') {
-                for (let [key, value] of Object.entries(commands[args[0]].fields)) {
+            if (typeof command.fields != 'undefined') {
+                for (let [key, value] of Object.entries(command.fields)) {
                     embed.addField(key, value, true);
                 }
             }
@@ -39,40 +43,34 @@ module.exports = function(bot, message) {
             message.channel.send({embed});
         }
     } else {
-        var helpPage = {};
-    
-        for (let [key, value] of Object.entries(commands)) {
-            if (typeof helpPage[value.category] == 'undefined') {
-                helpPage[value.category] = `**${prefix}${value.command}** : ${value.shortDesc}\n`;
-            } else {
-                helpPage[value.category] += `**${prefix}${value.command}** : ${value.shortDesc}\n`
-            }
-        }
-    
-        var embed = new Discord.RichEmbed()
-            .setAuthor(bot.user.username, bot.user.avatarURL)
-            .setTitle('Help Page')
-            .setColor('BLUE');
+        const user = new User(message.author);
+        user.init(() => {
+            var helpPage = {};
         
-    
-        var bitfield = 0;
-        if (typeof permissions[message.author.id] != 'undefined') {
-            bitfield = permissions[message.author.id];
-        } else {
-            bitfield = permissions.everyone;
-        }
-    
-        for (let [key, value] of Object.entries(helpPage)) {
-            if (key == 'Admin') {
-                if (UserPerms.decodePermsIntoArray(bitfield).indexOf('SHOW_ADMIN_COMMANDS') != -1) {
+            for (let [key, value] of Object.entries(commands)) {
+                if (typeof helpPage[value.category] == 'undefined') {
+                    helpPage[value.category] = `**${prefix}${key}** : ${value.shortDesc}\n`;
+                } else {
+                    helpPage[value.category] += `**${prefix}${key}** : ${value.shortDesc}\n`
+                }
+            }
+        
+            var embed = new Discord.RichEmbed()
+                .setTitle('Help Page')
+                .setColor('BLUE');
+        
+            for (let [key, value] of Object.entries(helpPage)) {
+                if (key == 'Admin') {
+                    if (user.perms.has('SHOW_ADMIN_COMMANDS')) {
+                        embed.addField(key, value);
+                    }
+                } else {
                     embed.addField(key, value);
                 }
-            } else {
-                embed.addField(key, value);
+        
             }
-    
-        }
-    
-        message.channel.send({embed});
+        
+            message.channel.send({embed});
+        })
     }
 }
