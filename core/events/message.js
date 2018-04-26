49b2471b6eafb17d943bc50d90e1b72bcea64d6c
Ciborn <rob1.bourachot@gmail.com>
2018-04-26 17:05:20 +0200

    fixed message handler preventing unknown commands

--- core/events/message.js
+++ core/events/message.js
@@ -14,47 +14,49 @@ module.exports = function(bot, message) {
                     const args = message.content.slice(guild.prefix.length).split(' ');
                     const command = new Command(args.shift());
 
-                    const execute = function(path) {
-                        try {
-                            require(`./../../${path}`)(bot, message);
-                        } catch (err) {
-                            const embed = new Discord.RichEmbed()
-                                .setTitle('An internal error occured')
-                                .setDescription(`Please report the following error the development team of **${message.author.username}**.\n\`\`\`xl\n${err}\`\`\``)
-                                .setColor('RED');
-                            message.channel.send({embed});
-                        }
-                    }
-
-                    if (user.perms.has('ADMINISTRATOR')) {
-                        execute(command.path);
-                    } else {
-                        var isAllowed = [false, false];
-                        var missingPerms = [[], []];
-                        for (let element of Perms.decodePermsIntoArray(command.botPerms)) {
-                            user.perms.has(element) ? isAllowed[0] = true : missingPerms[0].push(element);
-                        }
-
-                        if (command.discordPerms == '') {
-                            isAllowed[1] = true;
-                        } else {
-                            for (let element of command.discordPerms.split(' ')) {
-                                if (message.member.permissionsIn(message.channel).has(element)) {
-                                    isAllowed[1] = true;
-                                }
+                    if (command.botPerms != undefined) {
+                        const execute = function(path) {
+                            try {
+                                require(`./../../${path}`)(bot, message);
+                            } catch (err) {
+                                const embed = new Discord.RichEmbed()
+                                    .setTitle('An internal error occured')
+                                    .setDescription(`Please report the following error the development team of **${message.author.username}**.\n\`\`\`xl\n${err}\`\`\``)
+                                    .setColor('RED');
+                                message.channel.send({embed});
                             }
                         }
-
-                        if (isAllowed[0] && isAllowed[1]) {
+    
+                        if (user.perms.has('ADMINISTRATOR')) {
                             execute(command.path);
                         } else {
-                            const stringMissingPerms = `${!isEmpty(missingPerms[0]) ? `**${bot.user.username} Permissions : **` + missingPerms[0].join(' ') + '\n' : ''}${!isEmpty(missingPerms[1]) ? '**Discord Permissions : **' + missingPerms[1].join(' ') : ''}`;
-                            const embed = new Discord.RichEmbed()
-                                .setAuthor(message.author.username, message.author.avatarURL)
-                                .setTitle('Missing Permissions')
-                                .setDescription(`This command needs you to have permissions which you do not have, including the following : \n${stringMissingPerms}`)
-                                .setColor('ORANGE');
-                            message.channel.send({embed});
+                            var isAllowed = [false, false];
+                            var missingPerms = [[], []];
+                            for (let element of Perms.decodePermsIntoArray(command.botPerms)) {
+                                user.perms.has(element) ? isAllowed[0] = true : missingPerms[0].push(element);
+                            }
+    
+                            if (command.discordPerms == '') {
+                                isAllowed[1] = true;
+                            } else {
+                                for (let element of command.discordPerms.split(' ')) {
+                                    if (message.member.permissionsIn(message.channel).has(element)) {
+                                        isAllowed[1] = true;
+                                    }
+                                }
+                            }
+    
+                            if (isAllowed[0] && isAllowed[1]) {
+                                execute(command.path);
+                            } else {
+                                const stringMissingPerms = `${!isEmpty(missingPerms[0]) ? `**${bot.user.username} Permissions : **` + missingPerms[0].join(' ') + '\n' : ''}${!isEmpty(missingPerms[1]) ? '**Discord Permissions : **' + missingPerms[1].join(' ') : ''}`;
+                                const embed = new Discord.RichEmbed()
+                                    .setAuthor(message.author.username, message.author.avatarURL)
+                                    .setTitle('Missing Permissions')
+                                    .setDescription(`This command needs you to have permissions which you do not have, including the following : \n${stringMissingPerms}`)
+                                    .setColor('ORANGE');
+                                message.channel.send({embed});
+                            }
                         }
                     }
                 })
