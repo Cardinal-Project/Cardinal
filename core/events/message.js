f5f3be2bff59c50f4bdb5fc2053d3bfbef18e54b
Ciborn <rob1.bourachot@gmail.com>
2018-04-26 11:12:12 +0200

    updated message handler

--- core/events/message.js
+++ core/events/message.js
@@ -29,32 +29,30 @@ module.exports = function(bot, message) {
                     if (user.perms.has('ADMINISTRATOR')) {
                         execute(command.path);
                     } else {
-                        if (user.perms.has('USE_BOT')) {
-                            var isAllowed = [false, false];
-                            var missingPerms = [[], []];
-                            for (let element of Perms.decodePermsIntoArray(command.botPerms)) {
-                                user.perms.has(element) ? isAllowed[0] = true : missingPerms[0].push(element);
+                        var isAllowed = [false, false];
+                        var missingPerms = [[], []];
+                        for (let element of Perms.decodePermsIntoArray(command.botPerms)) {
+                            user.perms.has(element) ? isAllowed[0] = true : missingPerms[0].push(element);
+                        }
+
+                        if (command.discordPerms == '') {
+                            isAllowed[1] = true;
+                        } else {
+                            for (let element of Perms.decodePermsIntoArray(command.discordPerms)) {
+                                message.member.permissionsIn(message.channel).has(element) ? isAllowed[1] = true : missingPerms[1].push(element);
                             }
-    
-                            if (command.discordPerms == '') {
-                                isAllowed[1] = true;
-                            } else {
-                                for (let element of Perms.decodePermsIntoArray(command.discordPerms)) {
-                                    message.member.permissionsIn(message.channel).has(element) ? isAllowed[1] = true : missingPerms[1].push(element);
-                                }
-                            }
-    
-                            if (isAllowed[0] && isAllowed[1]) {
-                                execute(command.path);
-                            } else {
-                                const stringMissingPerms = `${!isEmpty(missingPerms[0]) ? `**${bot.user.username} Permissions : **` + missingPerms[0].join(' ') + '\n' : ''}${!isEmpty(missingPerms[1]) ? '**Discord Permissions : **' + missingPerms[1].join(' ') : ''}`;
-                                const embed = new Discord.RichEmbed()
-                                    .setAuthor(message.author.username, message.author.avatarURL)
-                                    .setTitle('Missing Permissions')
-                                    .setDescription(`This command needs you to have permissions which you do not have, including the following : \n${stringMissingPerms}`)
-                                    .setColor('ORANGE');
-                                message.channel.send({embed});
-                            }
+                        }
+
+                        if (isAllowed[0] && isAllowed[1]) {
+                            execute(command.path);
+                        } else {
+                            const stringMissingPerms = `${!isEmpty(missingPerms[0]) ? `**${bot.user.username} Permissions : **` + missingPerms[0].join(' ') + '\n' : ''}${!isEmpty(missingPerms[1]) ? '**Discord Permissions : **' + missingPerms[1].join(' ') : ''}`;
+                            const embed = new Discord.RichEmbed()
+                                .setAuthor(message.author.username, message.author.avatarURL)
+                                .setTitle('Missing Permissions')
+                                .setDescription(`This command needs you to have permissions which you do not have, including the following : \n${stringMissingPerms}`)
+                                .setColor('ORANGE');
+                            message.channel.send({embed});
                         }
                     }
                 })
