ad8da1f170e21c391cc0f9575b263f6e1f16fac0
Ciborn <rob1.bourachot@gmail.com>
2018-05-12 13:00:50 +0200

    remove useless formulas files

--- core/functions/formulas/XPToLevelUp.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(level, expo) {
-    expo = expo == undefined ? 2.1 : expo;
-    return Math.round(15 * Math.pow(level + 1, expo) - 15 * Math.pow(level, expo));
-}
--- core/functions/formulas/totalXP.js
+++
@@ -1,9 +0,0 @@
-const XPToLevelUp = require('./XPToLevelUp');
-module.exports = function(level, expo) {
-    var totalXP = 0;
-    do {
-        totalXP += XPToLevelUp(level, expo);
-        level--;
-    } while (level > 0);
-    return totalXP;
-}
