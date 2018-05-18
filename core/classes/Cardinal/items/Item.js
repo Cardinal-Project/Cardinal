2f94de54c968b50de0ab74315cc943c4ea30d61a
Ciborn <rob1.bourachot@gmail.com>
2018-05-18 11:20:33 +0200

    add Item class

---
+++ core/classes/Cardinal/items/Item.js
@@ -0,0 +1,45 @@
+const similarity = require('./../../../functions/utils/stringsSimilarity');
+const items = require('./../../../data/items.json');
+module.exports = class Item {
+    /**
+     * Builds the Item object
+     * @param item Name or ID of the item 
+     */
+    constructor(item) {
+        if (typeof item == 'number') {
+            this.id = item;
+        } else {
+            this.id = Item.findItem(item);
+        }
+        for (let [key, value] of Object.entries(items[this.id])) {
+            this[key] = value;
+        }
+    }
+
+    fetchItem() {
+        var item = items[this.id];
+        item.type = Object.keys(item)[2];
+        item.id = itemId;
+        return item;
+    }
+
+    static findItem(itemName) {
+        var allItems = new Map();
+        for (let [id, item] of Object.entries(items)) {
+            allItems.set(id, similarity(itemName, item.name));
+        }
+
+        allItems[Symbol.iterator] = function* () {
+            yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
+        }
+
+        var itemFound = 0;
+        for (let [key, value] of allItems) {
+            if (itemFound == 0) {
+                itemFound = key;
+            }
+        }
+
+        return itemFound;
+    }
+}
