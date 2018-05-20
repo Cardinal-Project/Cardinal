d3d655a2333abcf15787b93b26a3b93b38996c18
Robin Bourachot <rob1.bourachot@gmail.com>
2018-05-20 20:20:24 +0200

    add Equipment management in Player class

--- core/classes/Cardinal/Player.js
+++ core/classes/Cardinal/Player.js
@@ -2,6 +2,7 @@ const attributes = require('./../../functions/formulas/attributes');
 const fight = require('./../../functions/formulas/fight');
 const xp = require('./../../functions/formulas/xp');
 const Inventory = require('./items/Inventory');
+const Equipment = require('./items/Equipment');
 const Cache = require('./../Cache');
 // const Guild = require('./Guild');
 module.exports = class Player {
@@ -24,11 +25,12 @@ module.exports = class Player {
             this.level = xp.levelFromXP(this.xp)[0];
             this.xpEndLevel = xp.XPToLevelUp(this.level);
             this.prestige = cache.get('prestige');
+
             this.gold = cache.get('gold');
             this.hp = cache.get('hp');
             this.stamina = cache.get('stamina');
-
             this.inventory = new Inventory(this, cache.get('inventory'));
+            this.equipment = new Equipment(this, cache.get('equipment'));
 
             this.availablePoints = JSON.parse(cache.get('attributes')).availablePoints;
             this.attributes = JSON.parse(cache.get('attributes')).attributes;
@@ -36,7 +38,6 @@ module.exports = class Player {
             this.attributes.stamina = attributes.staminaFromVitality(this.attributes.vit);
 
             this.fight = this.fightData(this.attributes.stamina);
-
             this.skills = cache.get('skills');
             /*this.job = this.skills.job;
             this.jobXP = this.skills.job.xp;
