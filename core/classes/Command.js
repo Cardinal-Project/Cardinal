8b6cc5a6165dcc744947dcb748f81c14e62e5b33
Ciborn <rob1.bourachot@gmail.com>
2018-05-25 20:23:53 +0200

    update Command class

--- core/classes/Command.js
+++ core/classes/Command.js
@@ -1,12 +1,8 @@
-const commands = require('./../data/commands.json');
 module.exports = class Command {
     constructor(commandName) {
-        if (commands[commandName] != undefined) {
-            for (let [key, value] of Object.entries(commands[commandName])) {
-                this[key] = value;
-            }
-        } else {
-            return new TypeError(`Unknown Command`);
+        for (let [key, value] of Object.entries(require(`./../commands/${commandName}`).infos)) {
+            this[key] = value;
         }
+        this.command = commandName;
     }
 }
