38ad43842142153d51a2cd4e22aeebc55981a71d
Ciborn <rob1.bourachot@gmail.com>
2018-05-25 22:25:22 +0200

    make little changes to help command

--- core/commands/help.js
+++ core/commands/help.js
@@ -1,17 +1,9 @@
-const isEmpty = require('./../functions/utils/isEmpty');
 const commands = require('./../data/commands.json');
-const User = require('./../classes/Discord/User');
 const BotPerms = require('./../classes/BotPerms');
-const config = require('./../../config.json');
-const Cache = require('./../classes/Cache');
-const Args = require('./../classes/Args');
 const Discord = require('discord.js');
-module.exports = function(bot, message) {
-    const args = new Args(message.content, ' ');
+exports.run = function(bot, message, args, user) {
     args.args.shift();
-    const prefix = new Cache(message.guild.id, 'guildSettings.json').get('prefix');
-
-    if (!isEmpty(args.args)) {
+    if (!args.isEmpty()) {
         if (typeof commands[args.args[0]] != 'undefined') {
             const command = commands[args.args[0]];
             var embed = new Discord.RichEmbed()
@@ -20,13 +12,13 @@ module.exports = function(bot, message) {
                 .setThumbnail(bot.user.avatarURL)
                 .setDescription(command.shortDesc);
             
-            const requiredBotPerms = new BotPerms(command.botPerms);
+            const requiredBotPerms = new BotPerms(command.perms.bot);
             if (!requiredBotPerms.has('USE_BOT')) {
                 embed.addField('Required Bots Permissions', requiredBotPerms.perms.join(' '), true);
             }
             
-            if (command.discordPerms != '') {
-                embed.addField('Required Discord Permissions', command.discordPerms.trim(), true);
+            if (command.perms.discord != '') {
+                embed.addField('Required Discord Permissions', command.perms.discord.trim(), true);
             }
 
             if (typeof command.fields != 'undefined') {
@@ -39,39 +31,53 @@ module.exports = function(bot, message) {
         } else {
             const embed = new Discord.RichEmbed()
                 .setTitle(`Invalid Command Name`)
-                .setDescription(`The command name you gave does not match with a valid ${bot.user.username} command. Check **${prefix}help** to have a list of all commands.`)
+                .setDescription(`The command name you gave does not match with a valid ${bot.user.username} command.`)
                 .setColor('ORANGE');
             message.channel.send({embed});
         }
     } else {
-        const user = new User(message.author);
-        user.init(() => {
-            var helpPage = {};
-        
-            for (let [key, value] of Object.entries(commands)) {
-                if (typeof helpPage[value.category] == 'undefined') {
-                    helpPage[value.category] = `**${prefix}${key}** : ${value.shortDesc}\n`;
-                } else {
-                    helpPage[value.category] += `**${prefix}${key}** : ${value.shortDesc}\n`
-                }
+        var helpPage = {};
+    
+        for (let [key, value] of Object.entries(commands)) {
+            if (typeof helpPage[value.category] == 'undefined') {
+                helpPage[value.category] = `**${prefix}${key}** : ${value.shortDesc}\n`;
+            } else {
+                helpPage[value.category] += `**${prefix}${key}** : ${value.shortDesc}\n`
             }
-        
-            var embed = new Discord.RichEmbed()
-                .setTitle('Help Page')
-                .setColor('BLUE');
-        
-            for (let [key, value] of Object.entries(helpPage)) {
-                if (key == 'Admin') {
-                    if (user.perms.has('SHOW_ADMIN_COMMANDS')) {
-                        embed.addField(key, value);
-                    }
-                } else {
+        }
+    
+        var embed = new Discord.RichEmbed()
+            .setTitle('Help Page')
+            .setColor('BLUE');
+    
+        for (let [key, value] of Object.entries(helpPage)) {
+            if (key == 'Admin') {
+                if (user.perms.has('SHOW_ADMIN_COMMANDS')) {
                     embed.addField(key, value);
                 }
-        
+            } else {
+                embed.addField(key, value);
             }
-        
-            message.channel.send({embed});
-        })
+        }
+    
+        message.channel.send({embed});
+    }
+}
+
+exports.info = {
+    name: "Help Pages",
+    perms: {
+        bot: 1,
+        discord: null
+    },
+    enabled: `Command Re-Work In Progress`,
+    category: "Bot",
+    description: "Shows help pages",
+    args: {
+        1: {
+            types: ['string'],
+            desc: "`[*]` **Command Name**",
+            size: 1
+        }
     }
 }
